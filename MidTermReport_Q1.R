c1a375935f87accfc1821881adb9997013db07a4
Bryan Lo (TW) <30331982+LoChunhung@users.noreply.github.com>
2020-05-05 16:33:35 +0800

    期中考R程式碼
    紀念自己熬夜數天

---
+++ MidTermReport_Q1.R
@@ -0,0 +1,98 @@
+getwd()
+setwd("C:/Users/Chunhung.Lo/Documents/Projects/FJCU2020_RegressionAnalysis")
+# Section Tidy Data -Start
+#install.packages("readxl") # 安裝並載入可直接讀取excel的package
+library("readxl")
+UniStudent <- read_excel("108_student.xls",skip = 2)
+class(UniStudent) # 確認是否為特殊物件
+dfStud <- data.frame(UniStudent) # 調整為dataframe資料類型
+class(dfStud)
+rm(UniStudent) #刪除舊有資料集
+ls() #檢查現有的list物件
+
+for(i in 6:23){
+  dfStud[,i] <-gsub("-","0",dfStud[,i])
+  dfStud[,i]<-as.numeric(dfStud[,i])
+} #整理資料，將有關學生人數部分的欄位由文字型別轉為數值型別
+
+dfSD1 <- dfStud[(dfStud$"日間.進修別" == 'D 日' & dfStud$"體系別" == "1 一般"),]
+# subset(dfStud, (dfStud$"日間.進修別" == 'D 日' & dfStud$"體系別" == "1 一般")) #驗證輸出 
+#Section Tidy Data -End
+#Section- Question 1
+# Q1-1
+dfSD1_UG <- dfSD1[dfSD1$"等級別" == "B 學士",c("學校名稱","總計")]
+names(dfSD1_UG)[2] <- "學士班人數" #更名
+dfSD1_G <- dfSD1[dfSD1$"等級別" == "M 碩士",c("學校名稱","總計")]
+names(dfSD1_G)[2] <- "碩士班人數"  #更名
+Q11 <- merge(dfSD1_UG,dfSD1_G, by = "學校名稱")
+
+plot(Q11$"學士班人數", Q11$"碩士班人數" , xlab = "學士班人數", ylab = "碩士班人數")
+cor(Q11[,c(2,3)])
+#pearson 
+cor.test(Q11$"學士班人數", Q11$"碩士班人數")
+#spearman
+cor.test(Q11$"學士班人數", Q11$"碩士班人數", method = "spearman")
+
+#Q1-2 - 2個2元變數間的關係，使用 Phi correlation coefficient
+library(psych)
+#library(ltm)
+dfSD2 <- dfSD1[, c(1:2,4:7)]
+dfSD2["公私立"] = "私立"
+dfSD2[grepl(c("國立"),dfSD1$"學校名稱"), "公私立"] <- "公立"
+dfSD2[grepl(c("市立"),dfSD1$"學校名稱"), "公私立"] <- "公立"
+#相關係數(Phi correlation coefficient)
+dfPublicSex<-aggregate.data.frame(dfSD2[c("男生計","女生計")],by = list("公私立" = dfSD2$"公私立"),sum)
+rownames(dfPublicSex) = dfPublicSex$'公私立'
+dfPublicSex = dfPublicSex[-1]
+phi(dfPublicSex) #結果女性就讀私立學校的比率高於就讀公立學校呈現弱相關。
+
+
+#Q1-3 -欲了解台大,政大,清大,交大,輔大,銘傳,文化各校學生人數的結構差異
+#學士,碩士,博士的人數比例差異(不計在職專班)
+#試以合適的圖形呈現並解釋之
+#library(data.table)
+dfSD3<-subset(dfSD1,dfSD1$"等級別" != "X 4+X", 1:5)
+x <- c("0001","0002","0003","0007","1002","1006", "1016" )
+C6comp<-do.call(rbind,
+                lapply(x, function(x) subset(dfSD3 ,dfSD3$"學校代碼" == x, 1:5))
+                )
+
+y <- c("B 學士","M 碩士","D 博士")
+C6comp2<-do.call(cbind,
+                 lapply(y, function(y) subset(C6comp ,C6comp$"等級別" == y, c(2,5)))
+)
+C6comp2<-C6comp2[  c(-3,-5)]
+names(C6comp2)<- c("SchName","B","M","D")
+C6comp2["SchName"] <- c("政大","清大","台大","交大","輔大","文化","銘傳")
+barplot(cbind(B,M,D) ~ SchName, data = C6comp2,
+              legend = c("學","碩","博"),args.legend = list(x = "topleft")
+        ,ylim = c(0,40000), ylab = "總學生人數", xlab = "大學名稱")
+par(mfrow = c(2,4))
+pie(C6comp[7:9,]$"總計", labels = C6comp[7:9,]$"等級別", main = "台大")
+pie(C6comp[1:3,]$"總計", labels = C6comp[1:3,]$"等級別", main = "政大")
+pie(C6comp[4:6,]$"總計", labels = C6comp[4:6,]$"等級別", main = "清大")
+pie(C6comp[10:12,]$"總計", labels = C6comp[10:12,]$"等級別", main = "交大")
+pie(C6comp[13:15,]$"總計", labels = C6comp[13:15,]$"等級別", main = "輔大")
+pie(C6comp[16:18,]$"總計", labels = C6comp[16:18,]$"等級別", main = "文大")
+pie(C6comp[19:21,]$"總計", labels = C6comp[19:21,]$"等級別", main = "銘傳")
+# barplot(C6comp$"總計" ~ C6comp$"等級別", data = C6comp)
+# barplot(GNP ~ Year, data = longley)
+# View(longley)
+# barplot(cbind(Employed, Unemployed) ~ Year, data = longley)
+
+
+# Data tidy
+# method1:lapply and do.call()
+# method2: package:data.table , lappy and %>% 
+# C6comp <- (lapply(x, 
+#                   function(x) subset(dfStud,dfStud$"學校代碼" == x, 1:5)) %>% 
+#              do.call(rbind, .))
+#Q1-2 
+##觀念錯誤
+# biserial.cor(dfSD2$"男生計", dfSD2$"公私立")
+# biserial.cor(dfSD2$"女生計", dfSD2$"公私立")
+# dfSM<-aggregate.data.frame(dfSD2["男生計"],by = list("公私立" = dfSD2$"公私立", "學校名稱" = dfSD2$"學校名稱"),sum)
+# biserial.cor(dfSM$"男生計", dfSM$"公私立")
+# dfSF<-aggregate.data.frame(dfSD2["女生計"],by = list("公私立" = dfSD2$"公私立", "學校名稱" = dfSD2$"學校名稱"),sum)
+# biserial.cor(dfSF$"女生計", dfSF$"公私立")
+
---
+++ MidTermReport_Q2.R
@@ -0,0 +1,97 @@
+getwd()
+setwd("C:/Users/Chunhung.Lo/Documents/Projects/FJCU2020_RegressionAnalysis")
+# Section Tidy Data -Start
+#install.packages("readxl") # 安裝並載入可直接讀取excel的package
+library("readxl")
+UniStudent <- read_excel("108_student.xls",skip = 2)
+class(UniStudent) # 確認是否為特殊物件
+dfStud <- data.frame(UniStudent) # 調整為dataframe資料類型
+class(dfStud)
+rm(UniStudent) #刪除舊有資料集
+## Q2-1
+dfSD2_0 <- dfStud[(dfStud$"日間.進修別" == 'D 日' & dfStud$"體系別" == "1 一般"),c(1:5)]
+dfSD2_0["公私立"] = "私立"
+dfSD2_0[grepl(c("國立"),dfSD2_0$"學校名稱"), "公私立"] <- "公立"
+dfSD2_0[grepl(c("市立"),dfSD2_0$"學校名稱"), "公私立"] <- "公立"
+colnames(dfSD2_0)<-c("SchNo","SchName","DN","Degree","SSum","SchSYS")
+#fillist <- list("SchNo" = dfSD2_0$SchNo,"SchName" = dfSD2_0$SchName,"SchSYS"= dfSD2_0$SchSYS)
+dfSD2_1<- aggregate(SSum ~ SchSYS+SchName+SchNo,data =dfSD2_0,sum)
+
+#Data source 2- 取出資料來源 108_teacher.xls中的學校代碼與專任教師合計人數總計
+dfTRaw <- read_excel("108_teacher.xls",skip = 5, col_names = FALSE)
+dfTeacher <- dfTRaw[ (dfTRaw[3] == '日'& dfTRaw[26] == "1 一般"), c(1,4)]
+#Updated the name of variables.
+colnames(dfTeacher)<-c("SchNo","TSum")
+rm(dfTRaw)
+
+#Merge Data source 1 and 2 by SchNo(學校代號)
+dfStAndT<-merge.data.frame(dfSD2_1,dfTeacher, by = intersect(names(dfSD2_1),names(dfTeacher)))
+# 全國日間部學生/專任教師
+plot(dfStAndT$SSum,dfStAndT$TSum)
+cor.test(dfStAndT$SSum,dfStAndT$TSum) # Pearson's corr is 0.8673092
+cor.test(dfStAndT$SSum,dfStAndT$TSum, method = "spearman") #Spearman's rho 0.9048077 
+#公私立系統比較
+cor.test(dfStAndT[dfStAndT$SchSYS == "公立", "SSum"],dfStAndT[dfStAndT$SchSYS == "公立", "TSum"])
+plot(dfStAndT[dfStAndT$SchSYS == "公立", "SSum"],dfStAndT[dfStAndT$SchSYS == "公立", "TSum"])
+cor.test(dfStAndT[dfStAndT$SchSYS == "私立", "SSum"],dfStAndT[dfStAndT$SchSYS == "私立", "TSum"])
+plot(dfStAndT[dfStAndT$SchSYS == "私立", "SSum"],dfStAndT[dfStAndT$SchSYS == "私立", "TSum"])
+#########
+## Q2-2 #
+#########
+
+lm(formula= SSum~ TSum, dfStAndT)
+SimpleSTModel<-lm(formula= SSum~ TSum, dfStAndT)
+plot(SSum~ TSum, dfStAndT)
+abline(coef(SimpleSTModel))
+#########
+## Q2-3 #
+#########
+lm(formula= SSum~ TSum, dfStAndT[dfStAndT$SchSYS == "公立",])
+lm(formula= SSum~ TSum, dfStAndT[dfStAndT$SchSYS == "私立",])
+
+#########
+## Q2-4 #
+#########
+
+#########
+## Q2-5 #
+#########
+# Data Tidy- Staff
+dfStaff <- read_excel("108_staff.xls",skip = 2)
+dfStaff <-dfStaff[dfStaff$"體系別" == "1 一般",c(1,3)]
+colnames(dfStaff)<-c("SchNo","StfSum")
+# Data Tidy- Books
+dfBooks <- read_excel("108_library.xls", sheet = "校別資料",skip = 10,col_names = TRUE)
+dfBooks <-dfBooks[c(1,4)]
+colnames(dfBooks)<-c("SchNo","BookSum")
+# Data Tidy- BuildingAreas
+dfLands <-read_excel("108_land.xls",skip = 8)
+dfLands <-dfLands[,c(1,4)]
+colnames(dfLands)<-c("SchNo","BldAreaSum")
+
+for (i in 1:dim(dfLands)[1]) {
+  if (is.na(dfLands[i,"SchNo"])) {
+    #print(dfLands[i,"SchNo"])
+    dfLands[i,"SchNo"] <- dfLands[i-1,"SchNo"]
+  }
+  #print(i)
+}
+
+#Linear Multi Regression
+dfLands_1<- aggregate(BldAreaSum ~ SchNo,data =dfLands,sum)
+dfAll<-merge.data.frame(dfStAndT,dfStaff, by = intersect(names(dfStAndT),names(dfStaff)))
+dfAll<-merge.data.frame(dfAll,dfBooks, by = intersect(names(dfStAndT),names(dfBooks)))
+dfAll<-merge.data.frame(dfAll,dfLands_1, by = intersect(names(dfStAndT),names(dfLands_1)))
+lm(SSum~TSum+StfSum+BookSum+BldAreaSum,data= dfAll)
+
+
+# 誤讀老師題目，需刪除教官後其他人員，後經由教育部資料定義發現誤會
+# 保留紀念自己的中文能力拙劣
+# dfTeacher <- dfTRaw[ (dfTRaw[3] == '日'& dfTRaw[26] == "1 一般")
+#                      ,c(1:4,15:26)]
+# dfTeacher["TSumR"]<-dfTeacher["TSum"]- data.frame(rowSums(dfTeacher[,c("M1","F1","M2","F2","M3","F3","M4","F4")], na.rm = FALSE, dims = 1))
+# dfLands <-read.csv("108_land.csv",header= FALSE,skip = 8,stringsAsFactors = FALSE,col.names= c(1:12), fileEncoding="UTF8")
+# dfLands_1 <- aggregate()
+
+
+
